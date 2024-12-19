06140ec058e7d5c4eb6403b4a70bba141f1648d3
M Asad <muhammad.asad@qmul.ac.uk>
2024-12-19 08:15:29 +0000

    fixed run scripts
    Signed-off-by: M Asad <muhammad.asad@qmul.ac.uk>

--- runx4.sh
+++ runx4.sh
@@ -14,4 +14,4 @@ pip install torch==1.7.1+cu110 torchvision==0.8.2+cu110 torchaudio==0.7.2 -f htt
 python -c 'import torch; print(torch.cuda.is_available())'
 cd /data/home/xaw004/Codes/MICCAI2025/SR_MRI_MI
 pip install -r requirements.txt
-python3 train.py --config configs/config.json --model idn --content_loss L1 --scale 4 --dataset_path /data3/scratch/xaw004/MICCAI2025Data/fastMRI
+python3 training.py --config configs/config.json --model idn --content_loss L1 --scale 4 --dataset_path /data3/scratch/xaw004/MICCAI2025Data/fastMRI
